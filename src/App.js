a202d534fe5dffb024d80ddef05354515f234905
chrisdotjs <chrisdotjs@gmail.com>
2022-12-23 16:24:07 +0100

    Start static UI and props

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
+import React, { useState, UseEffect } from 'react';
 import './App.css';
 
-function App() {
+const PRODUCTS = [
+  {category: "Fruits", price: "$1", stocked: true, name: "Apple"},
+  {category: "Fruits", price: "$1", stocked: true, name: "Dragonfruit"},
+  {category: "Fruits", price: "$2", stocked: false, name: "Passionfruit"},
+  {category: "Vegetables", price: "$2", stocked: true, name: "Spinach"},
+  {category: "Vegetables", price: "$4", stocked: false, name: "Pumpkin"},
+  {category: "Vegetables", price: "$1", stocked: true, name: "Peas"}
+]
+
+function SearchBar() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <div>SearchBar</div>
+  )
 }
 
-export default App;
+function ProductCategoryRow() {
+  return (
+    <div>ProductCategoryRow</div>
+  )
+}
+
+function ProductRow() {
+  return (
+    <div>ProductRow</div>
+  )
+}
+
+function ProductTable({products}) {
+
+  return (
+    <div>
+      ProductTable
+      <ProductCategoryRow />
+      <ProductRow />
+    </div>
+  )
+}
+
+function FilterableProductTable({products}) {
+  return (
+    <div>
+      FilterableProductTable
+      <SearchBar />
+      <ProductTable products={products} />
+    </div>
+  )
+}
+
+function App() {
+  return <FilterableProductTable products={PRODUCTS} />
+}
+
+export default App
